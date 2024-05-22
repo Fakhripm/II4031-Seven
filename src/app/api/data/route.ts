29385de9e2c9e1e8389687cb4e7a66e3bc177016
18221171 <18221171@std.stei.itb.ac.id>
2024-05-22 12:27:08 +0700

    feat: tabel akademik

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,29 +1,28 @@
-import { NextRequest,NextResponse } from "next/server";
-import { supabase } from '../../../../utils/supabase/client';
+import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "@/utils/supabase/client";
 
-export const GET = async (req: NextRequest, res: Response) => {
+export const GET = async (req: NextRequest) => {
   try {
-      const { data, error } = await supabase.from("akademik").select("*");
-      if (error) {
-          throw error;
-      }
-      return NextResponse.json({ data }, { status: 200 });
+    const { data, error } = await supabase.from("akademik").select("*");
+    if (error) {
+      throw error;
+    }
+    return NextResponse.json({ data }, { status: 200 });
   } catch (err) {
-      return NextResponse.json({message : "err" }, { status: 200 });
+    return NextResponse.json({ message: "err" }, { status: 200 });
   }
 };
 
-
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
   try {
-    const body = await req.json(); 
+    const body = await req.json();
     const struct = {
-      key1: body.key1 ,
-      key2: body.key2
+      key1: body.key1,
+      key2: body.key2,
     };
 
     return NextResponse.json({ message: "post", struct }, { status: 201 });
   } catch (err) {
-    return NextResponse.json({ message: "Error", err}, { status: 500 });
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,145 +1,87 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { hitungIPK } from "@/utils/indeks";
-
-interface MataKuliah {
-  kodeMK: string;
-  namaMK: string;
-  nilaiMK: string;
-  sksMK: number;
-}
-
-interface RecordData {
-  nim: string;
-  nama: string;
-  mk: MataKuliah[];
-  ttd: string;
-}
+import { GET, POST } from "./api/data/route";
+import { NextRequest } from "next/server";
+import InputData from "./Form";
+import { Akademik } from "./api/data/mahasiswa/[nim]/route";
 
 async function getAcademicData() {
-  const res = await fetch("");
+  const req = new NextRequest(new URL("/api/data", "http://localhost:3000"));
+  const res = await GET(req);
+
   if (!res.ok) {
     throw new Error("getAcademicData: Failed to Fetch Data");
   }
-  return res.json;
+
+  const data = await res.json();
+  return data.data;
 }
 
-const data1: RecordData = {
-  nim: "19223345",
-  nama: "John Doe",
-  mk: [
-    { kodeMK: "MK001", namaMK: "Fisika", nilaiMK: "B", sksMK: 3 },
-    { kodeMK: "MK002", namaMK: "Kimia", nilaiMK: "A", sksMK: 2 },
-    { kodeMK: "MK003", namaMK: "Biologi", nilaiMK: "B", sksMK: 3 },
-    { kodeMK: "MK004", namaMK: "Matematika", nilaiMK: "C", sksMK: 2 },
-    { kodeMK: "MK005", namaMK: "Bahasa Inggris", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK006", namaMK: "Sejarah", nilaiMK: "B", sksMK: 2 },
-    { kodeMK: "MK007", namaMK: "Ekonomi", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK008", namaMK: "Sosiologi", nilaiMK: "C", sksMK: 2 },
-    { kodeMK: "MK009", namaMK: "Seni Budaya", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK010", namaMK: "Tata Boga", nilaiMK: "B", sksMK: 2 },
-  ],
-  ttd: "opqrstuvwxyz",
-};
-
-const data2: RecordData = {
-  nim: "18221171",
-  nama: "Hans Stephano Edbert N",
-  mk: [
-    { kodeMK: "MK001", namaMK: "Matematika", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK002", namaMK: "Fisika", nilaiMK: "B", sksMK: 2 },
-    { kodeMK: "MK003", namaMK: "Bahasa Indonesia", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK004", namaMK: "Kimia", nilaiMK: "C", sksMK: 2 },
-    { kodeMK: "MK005", namaMK: "Biologi", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK006", namaMK: "Bahasa Inggris", nilaiMK: "B", sksMK: 2 },
-    { kodeMK: "MK007", namaMK: "Sejarah", nilaiMK: "B", sksMK: 3 },
-    { kodeMK: "MK008", namaMK: "Ekonomi", nilaiMK: "C", sksMK: 2 },
-    { kodeMK: "MK009", namaMK: "Sosiologi", nilaiMK: "A", sksMK: 3 },
-    { kodeMK: "MK010", namaMK: "Seni Budaya", nilaiMK: "A", sksMK: 2 },
-  ],
-  ttd: "abcdefghijklmn",
-};
-
-const Record = (record: RecordData) => {
+const Record = (data: Akademik) => {
   return (
     <tr>
-      <td>{record.nim}</td>
-      <td>{record.nama}</td>
-      {record.mk.map((matkul, index) => (
-        <React.Fragment key={index}>
-          <td>{matkul.kodeMK}</td>
-          <td>{matkul.namaMK}</td>
-          <td>{matkul.nilaiMK}</td>
-          <td>{matkul.sksMK}</td>
-        </React.Fragment>
-      ))}
-      <td>{hitungIPK(record.mk)}</td>
-      <td>{record.ttd}</td>
+      <td>{data.NIM}</td>
+      <td>{data.nama}</td>
+      <td>{data.kode_mk1}</td>
+      <td>{data.nama_matkul1}</td>
+      <td>{data.nilai1}</td>
+      <td>{data.sks1}</td>
+      <td>{data.kode_mk2}</td>
+      <td>{data.nama_matkul2}</td>
+      <td>{data.nilai2}</td>
+      <td>{data.sks2}</td>
+      <td>{data.kode_mk3}</td>
+      <td>{data.nama_matkul3}</td>
+      <td>{data.nilai3}</td>
+      <td>{data.sks3}</td>
+      <td>{data.kode_mk4}</td>
+      <td>{data.nama_matkul4}</td>
+      <td>{data.nilai4}</td>
+      <td>{data.sks4}</td>
+      <td>{data.kode_mk5}</td>
+      <td>{data.nama_matkul5}</td>
+      <td>{data.nilai5}</td>
+      <td>{data.sks5}</td>
+      <td>{data.kode_mk6}</td>
+      <td>{data.nama_matkul6}</td>
+      <td>{data.nilai6}</td>
+      <td>{data.sks6}</td>
+      <td>{data.kode_mk7}</td>
+      <td>{data.nama_matkul7}</td>
+      <td>{data.nilai7}</td>
+      <td>{data.sks7}</td>
+      <td>{data.kode_mk8}</td>
+      <td>{data.nama_matkul8}</td>
+      <td>{data.nilai8}</td>
+      <td>{data.sks8}</td>
+      <td>{data.kode_mk9}</td>
+      <td>{data.nama_matkul9}</td>
+      <td>{data.nilai9}</td>
+      <td>{data.sks9}</td>
+      <td>{data.kode_mk10}</td>
+      <td>{data.nama_matkul10}</td>
+      <td>{data.nilai10}</td>
+      <td>{data.sks10}</td>
+      <td>{data.ipk}</td>
+      <td>{data.ttd}</td>
     </tr>
   );
 };
 
-const InputMataKuliah = ({ index }: { index: string }) => (
-  <div className="flex w-full gap-4">
-    <text className="min-w-28 font-semibold">Mata Kuliah {index}</text>
-    <input
-      name="kode"
-      minLength={6}
-      maxLength={6}
-      placeholder="Kode Mata Kuliah"
-      required
-    />
-    <input name="nama" placeholder="Nama Mata Kuliah" required />
-    <input
-      name="nilai"
-      placeholder="Nilai Mata Kuliah"
-      required
-      pattern="[Aa][Bb]?|[Bb][Cc]?|[Cc]|[Dd]|[Ee]"
-    />
-    <input
-      name="sks"
-      type="number"
-      max={9}
-      placeholder="SKS Mata Kuliah"
-      required
-    />
-  </div>
-);
-
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-
-  const recordData: RecordData[] = [];
-  const formData = new FormData(e.currentTarget);
-
-  const nim = formData.get("nim-input");
-  const nama = formData.get("nama-input");
-  const kodeMK = formData.getAll("kode");
-  const namaMK = formData.getAll("nama");
-  const nilaiMK = formData.getAll("nilai");
-  const sksMK = formData.getAll("sks");
-
-  console.log(nim);
-  console.log(nama);
-  console.log(kodeMK);
-  console.log(namaMK);
-  console.log(nilaiMK);
-  console.log(sksMK);
-
-  if (kodeMK.length !== 10) {
-    alert("Please fill the empty fields for 'Kode Mata Kuliah'!");
-  } else if (namaMK.length !== 10) {
-    alert("Please fill the empty fields for 'Nama Mata Kuliah'!");
-  } else if (nilaiMK.length !== 10) {
-    alert("Please fill the empty fields for 'Nilai Mata Kuliah'!");
-  } else if (sksMK.length !== 10) {
-    alert("Please fill the empty fields for 'SKS Mata Kuliah'!");
-  } else {
-    // sendInputValueToApi(inputValue).then(() => /* Do something */)
-  }
-};
-
 export default function Page() {
+  const [data, setData] = useState(Array<Akademik>());
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getAcademicData();
+      console.log(result);
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex flex-col px-8 py-8">
       <header className="flex w-full flex-col items-center gap-6 py-4">
@@ -190,59 +132,20 @@ export default function Page() {
               </tr>
             </thead>
             <tbody className="text-center">
-              <Record
-                nama={data1.nama}
-                nim={data1.nim}
-                mk={data1.mk}
-                ttd={data1.ttd}
-              />
-              <Record
-                nama={data2.nama}
-                nim={data2.nim}
-                mk={data2.mk}
-                ttd={data2.ttd}
-              />
+              {data ? (
+                data.map((record) => <Record key={record.id} {...record} />)
+              ) : (
+                <tr>
+                  <td colSpan={46}>
+                    No data available
+                  </td>
+                </tr>
+              )}
             </tbody>
           </table>
         </article>
       </section>
-      <form onSubmit={handleSubmit}>
-        <section className="flex w-full pb-2">
-          <aside className="flex w-1/5 flex-col py-4 pr-4">
-            <h2 className="font-semibold italic">Input Data Diri</h2>
-            <label htmlFor="nim-input" className="block pb-1 pt-2 font-medium">
-              NIM
-            </label>
-            <input id="nim-input" name="nim-input" placeholder="Masukkan NIM" />
-            <label htmlFor="nama-input" className="block pb-1 pt-2 font-medium">
-              Nama Mahasiswa
-            </label>
-            <input
-              id="nama-input"
-              name="nama-input"
-              placeholder="Masukkan Nama Mahasiswa"
-            />
-          </aside>
-          <section className="flex w-4/5 flex-col gap-2 overflow-x-auto py-4 pl-4">
-            <h2 className="pb-1 font-semibold italic">Input Mata Kuliah</h2>
-            <InputMataKuliah index="01" />
-            <InputMataKuliah index="02" />
-            <InputMataKuliah index="03" />
-            <InputMataKuliah index="04" />
-            <InputMataKuliah index="05" />
-            <InputMataKuliah index="06" />
-            <InputMataKuliah index="07" />
-            <InputMataKuliah index="08" />
-            <InputMataKuliah index="09" />
-            <InputMataKuliah index="10" />
-          </section>
-        </section>
-
-        <input
-          type="submit"
-          className="w-full rounded-md bg-gray-800 py-1 font-medium italic text-white hover:cursor-pointer hover:bg-black"
-        />
-      </form>
+      <InputData />
     </div>
   );
 }
